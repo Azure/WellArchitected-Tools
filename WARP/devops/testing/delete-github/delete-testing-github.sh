94353218b1c28e28589ebeef0758d57f0c9b221b
rspott@cspott.com <rspott@cspott.com>
2021-12-30 12:36:44 -0700

    Rewrite of the github insert script
    Some extra files.
    test-assessment.csv is the result of clicking "none of the above" on every section of the assesment.
    test-assessmentsmall.csv is every 4th line in the test-assessment.csv file.
    delete-testing-github.sh is a bash script that will destroy your github repo and then create a new empty one with the same name when it is finished.

--- WARP/devops/testing/delete-github/delete-testing-github.sh
+++ WARP/devops/testing/delete-github/delete-testing-github.sh
@@ -1,62 +1,28 @@
 GITHUB_OWNER=rspott
-GITHUB_TOKEN=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
+GITHUB_TOKEN=$1
 GITHUB_PROJECT=WAF-test02
 
 GITHUB_OWNER_ESC="\\\"$GITHUB_OWNER\\\""
 GITHUB_PROJECT_ESC="\\\"$GITHUB_PROJECT\\\""
 
-#find all the open issues and then delete them.
-LOOKUP='query {
-  viewer {
-    id
-  }
-  repository(name: $GITHUB_PROJECT_ESC, owner: $GITHUB_OWNER_ESC) {
-    issues(filterBy: {states: OPEN}, first: 100) {
-      edges {
-        node {
-          id
-        }
-      }
-    }
-  }
-}'
-LOOKUP="$(echo $LOOKUP)"   # the query should be a one-liner, without newlines
-COUNT=1
-for ISSUE in $(curl -s -H 'Content-Type: application/json' \
-    -H "Authorization: bearer $GITHUB_TOKEN" \
-    -X POST -d "{ \"query\": \"$LOOKUP\"}" https://api.github.com/graphql | jq --raw-output '.data.repository.issues.edges[].node.id' | sed 's/= /\n/g')
-do
-    echo $COUNT
-    COUNT=$((COUNT+1))
+DATA='{"name":"'
+DATA+=$GITHUB_PROJECT
+DATA+='"}'
 
-    ISSUE="\\\"$ISSUE\\\""
+# Delete the entire repo
+curl \
+  -i -X DELETE \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+   https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_PROJECT}
 
-    DELETE="mutation {
-        deleteIssue(input: {issueId: $ISSUE}) {
-            clientMutationId
-        }
-    }"
+sleep 5
 
-    DELETE="$(echo $DELETE)" # the query should be a one-liner, without newlines
-
-    curl -s -H 'Content-Type: application/json' \
-    -H "Authorization: bearer $GITHUB_TOKEN" \
-    -X POST -d "{ \"query\": \"$DELETE\"}" https://api.github.com/graphql
-done
-
-#find all the labels we may have created and delete them
-while read LABELS; do
-  LABELS=${LABELS// /%20}
-  echo "$LABELS"
-  curl -i -u $GITHUB_OWNER:$GITHUB_TOKEN -X DELETE "https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_PROJECT/labels/$LABELS"
-
-done <labels.txt
-
-# Delete all the milestones
-for MILESTONE in {1..30}
-do
-  echo $MILESTONE
-   curl -i -u $GITHUB_OWNER:$GITHUB_TOKEN -X DELETE "https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_PROJECT/milestones/$MILESTONE"
-done
-
+# create a new one!
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  https://api.github.com/user/repos \
+  -d "$DATA"
 
