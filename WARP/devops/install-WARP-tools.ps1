9638c3afc5a2be8e10c79ef357cc0889aadc93fa
Daniel Margetic <63709585+danielmamsft@users.noreply.github.com>
2024-11-29 15:49:01 +0100

    Update install-WARP-tools.ps1
    Update this instance of install-WARP-tools.ps1 to point to the current devops folder.

--- WARP/devops/install-WARP-tools.ps1
+++ WARP/devops/install-WARP-tools.ps1
@@ -5,7 +5,7 @@ param(
 #This script will download a list of files from the proper location to the directory it is running from
 
 #This is the base URL for downloads. Base URL cannot end with a /
-$baseURL = "https://raw.githubusercontent.com/Azure/WellArchitected-Tools/$Branch/WARP"
+$baseURL = "https://raw.githubusercontent.com/Azure/WellArchitected-Tools/$Branch/WARP/devops"
 
 $workingDirectory = (Get-Location).Path
 Write-Host "Working Directory: $workingDirectory"
